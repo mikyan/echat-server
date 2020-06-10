2564d03a6763c4e5b4d175e323f50c88b1c7c498
mikyan <1677684179@qq.com>
2020-06-10 17:25:33 +0800

    一次迭代完成

--- src/main/java/cn/mikyan/controller/UserController.java
+++ src/main/java/cn/mikyan/controller/UserController.java
@@ -1,5 +1,7 @@
 package cn.mikyan.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
@@ -24,7 +26,6 @@ import cn.mikyan.utils.FileUtils;
 import cn.mikyan.controller.pojo.ResponseJSON;
 import cn.mikyan.utils.MD5Utils;
 import cn.mikyan.controller.MyAnnotation.*;
-import cn.mikyan.controller.pojo.*;
 @RestController
 @RequestMapping("v1/u")
 
@@ -47,7 +48,7 @@ public class UserController {
         return null;
     }
     
-    // @Autowired
+    @Autowired
 	private UserService userService;
 	
 	@Autowired
@@ -58,9 +59,36 @@ public class UserController {
 	 */
 	@PostMapping("/registOrLogin")
 	public ResponseJSON registOrLogin(HttpServletResponse response,@RequestBody Users user) throws Exception {
+		// 0. 判断用户名和密码不能为空
+		if (StringUtils.isBlank(user.getUsername()) 
+				|| StringUtils.isBlank(user.getPassword())) {
+			return ResponseJSON.errorMsg("用户名或密码不能为空...");
+		}
 		
+		// 1. 判断用户名是否存在，如果存在就登录，如果不存在则注册
+		boolean usernameIsExist = userService.queryUsernameIsExist(user.getUsername());
+		Users userResult = null;
+		if (usernameIsExist) {
+			// 1.1 登录
+			userResult = userService.queryUserForLogin(user.getUsername(), 
+									MD5Utils.getMD5Str(user.getPassword()));
+			if (userResult == null) {
+				return ResponseJSON.errorMsg("用户名或密码不正确..."); 
+			}
+		} else {
+			// 1.2 注册
+			user.setNickName(user.getUsername());
+			user.setFaceimage("");
+			user.setFaceimageBig("");
+			user.setPassword(MD5Utils.getMD5Str(user.getPassword()));
+			userResult = userService.saveUser(user);
+		}
+		
+		UsersVO userVO = new UsersVO();
+		BeanUtils.copyProperties(userResult, userVO);
+		
+		return ResponseJSON.ok(userVO);
 		// TODO 
-		return null;
 	}
 	
 	/**
@@ -104,8 +132,33 @@ public class UserController {
 	 */
 	@PostMapping("/uploadFaceBase64")
 	public ResponseJSON uploadFaceBase64(@RequestBody UsersBO userBO) throws Exception {
-		//TODO
-		return null;
+		// 获取前端传过来的base64字符串, 然后转换为文件对象再上传
+		String base64Data = userBO.getFaceData();
+		String userFacePath = "C:\\" + userBO.getUserId() + "userface64.png";
+		FileUtils.base64ToFile(userFacePath, base64Data);
+		
+		// 上传文件到fastdfs
+		MultipartFile faceFile = FileUtils.fileToMultipart(userFacePath);
+		String url = fastDFSClient.uploadBase64(faceFile);
+		System.out.println(url);
+		
+//		"dhawuidhwaiuh3u89u98432.png"
+//		"dhawuidhwaiuh3u89u98432_80x80.png"
+		
+		// 获取缩略图的url
+		String thump = "_80x80.";
+		String arr[] = url.split("\\.");
+		String thumpImgUrl = arr[0] + thump + arr[1];
+		
+		// 更新用户头像
+		Users user = new Users();
+		user.setId(userBO.getUserId());
+		user.setFaceimage(thumpImgUrl);
+		user.setFaceimageBig(url);
+		
+		Users result = userService.updateUserInfo(user);
+		
+		return ResponseJSON.ok(result);
 	}
 
 
@@ -115,8 +168,13 @@ public class UserController {
 	@PostMapping("/setNickname")
 	public ResponseJSON setNickname(@RequestBody UsersBO userBO) throws Exception {
         
-        // TODO
-        return null;
+        Users user = new Users();
+		user.setId(userBO.getUserId());
+		user.setNickName(userBO.getNickname());
+		
+		Users result = userService.updateUserInfo(user);
+		
+		return ResponseJSON.ok(result);
 	}
 	
 	/**
@@ -125,9 +183,26 @@ public class UserController {
 	@PostMapping("/search")
 	public ResponseJSON searchUser(String myUserId, String friendUsername)
 			throws Exception {
-        // TODO
         
-		return null;
+		// 0. 判断 myUserId friendUsername 不能为空
+		if (StringUtils.isBlank(myUserId) 
+				|| StringUtils.isBlank(friendUsername)) {
+			return ResponseJSON.errorMsg("");
+		}
+		
+		// 前置条件 - 1. 搜索的用户如果不存在，返回[无此用户]
+		// 前置条件 - 2. 搜索账号是你自己，返回[不能添加自己]
+		// 前置条件 - 3. 搜索的朋友已经是你的好友，返回[该用户已经是你的好友]
+		Integer status = userService.preconditionSearchFriends(myUserId, friendUsername);
+		if (status == SearchFriendsStatusEnum.SUCCESS.status) {
+			Users user = userService.queryUserInfoByUsername(friendUsername);
+			UsersVO userVO = new UsersVO();
+			BeanUtils.copyProperties(user, userVO);
+			return ResponseJSON.ok(userVO);
+		} else {
+			String errorMsg = SearchFriendsStatusEnum.getMsgByKey(status);
+			return ResponseJSON.errorMsg(errorMsg);
+		}
 		
 	}
 	
@@ -139,8 +214,24 @@ public class UserController {
 	public ResponseJSON addFriendRequest(String myUserId, String friendUsername)
 			throws Exception {
 		
-		// TODO 
-		return null;
+		// 0. 判断 myUserId friendUsername 不能为空
+		if (StringUtils.isBlank(myUserId) 
+				|| StringUtils.isBlank(friendUsername)) {
+			return ResponseJSON.errorMsg("");
+		}
+		
+		// 前置条件 - 1. 搜索的用户如果不存在，返回[无此用户]
+		// 前置条件 - 2. 搜索账号是你自己，返回[不能添加自己]
+		// 前置条件 - 3. 搜索的朋友已经是你的好友，返回[该用户已经是你的好友]
+		Integer status = userService.preconditionSearchFriends(myUserId, friendUsername);
+		if (status == SearchFriendsStatusEnum.SUCCESS.status) {
+			userService.sendFriendRequest(myUserId, friendUsername);
+		} else {
+			String errorMsg = SearchFriendsStatusEnum.getMsgByKey(status);
+			return ResponseJSON.errorMsg(errorMsg);
+		}
+		
+		return ResponseJSON.ok();
 	}
 	
 	/**
@@ -149,8 +240,13 @@ public class UserController {
 	@PostMapping("/queryFriendRequests")
 	public ResponseJSON queryFriendRequests(String userId) {
 		
-		// TODO 
-		return null;
+		// 0. 判断不能为空
+		if (StringUtils.isBlank(userId)) {
+			return ResponseJSON.errorMsg("");
+		}
+		
+		// 1. 查询用户接受到的朋友申请
+		return ResponseJSON.ok(userService.queryFriendRequestList(userId));
 	}
 	
 	
@@ -161,8 +257,31 @@ public class UserController {
 	public ResponseJSON operatorFriendRequest(String acceptUserId, String sendUserId,
 												Integer operType) {
 		
-		// TODO 
-		return null;
+		// 0. acceptUserId sendUserId operType 判断不能为空
+		if (StringUtils.isBlank(acceptUserId) 
+				|| StringUtils.isBlank(sendUserId) 
+				|| operType == null) {
+			return ResponseJSON.errorMsg("");
+		}
+		
+		// 1. 如果operType 没有对应的枚举值，则直接抛出空错误信息
+		if (StringUtils.isBlank(FriendRequestOperatorEnum.getMsgByType(operType))) {
+			return ResponseJSON.errorMsg("");
+		}
+		
+		if (operType == FriendRequestOperatorEnum.IGNORE.type) {
+			// 2. 判断如果忽略好友请求，则直接删除好友请求的数据库表记录
+			userService.deleteFriendRequest(sendUserId, acceptUserId);
+		} else if (operType == FriendRequestOperatorEnum.PASS.type) {
+			// 3. 判断如果是通过好友请求，则互相增加好友记录到数据库对应的表
+			//	   然后删除好友请求的数据库表记录
+			userService.passFriendRequest(sendUserId, acceptUserId);
+		}
+		
+		// 4. 数据库查询好友列表
+		List<MyFriendsVO> myFirends = userService.queryMyFriends(acceptUserId);
+		
+		return ResponseJSON.ok(myFirends);
 	}
 	
 	/**
@@ -170,8 +289,15 @@ public class UserController {
 	 */
 	@PostMapping("/myFriends")
 	public ResponseJSON myFriends(String userId) {
-		// TODO 
-		return null;
+		// 0. userId 判断不能为空
+		if (StringUtils.isBlank(userId)) {
+			return ResponseJSON.errorMsg("");
+		}
+		
+		// 1. 数据库查询好友列表
+		List<MyFriendsVO> myFirends = userService.queryMyFriends(userId);
+		
+		return ResponseJSON.ok(myFirends);
 	}
 	
 	/**
@@ -180,7 +306,14 @@ public class UserController {
 	 */
 	@PostMapping("/getUnReadMsgList")
 	public ResponseJSON getUnReadMsgList(String acceptUserId) {
-		// TODO 
-		return null;
+		// 0. userId 判断不能为空
+		if (StringUtils.isBlank(acceptUserId)) {
+			return ResponseJSON.errorMsg("");
+		}
+		
+		// 查询列表
+		List<cn.mikyan.pojo.UsersChatMsg> unreadMsgList = userService.getUnReadMsgList(acceptUserId);
+		
+		return ResponseJSON.ok(unreadMsgList);
 	}
 }
--- src/main/java/cn/mikyan/netty/ChatHandler.java
+++ src/main/java/cn/mikyan/netty/ChatHandler.java
@@ -1,13 +1,23 @@
 package cn.mikyan.netty;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import cn.mikyan.SpringUtil;
+import cn.mikyan.enums.MsgActionEnum;
+import cn.mikyan.netty.pojo.ChatMsg;
+import cn.mikyan.netty.pojo.DataContent;
+import cn.mikyan.service.UserService;
+import cn.mikyan.utils.JsonUtils;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.util.concurrent.GlobalEventExecutor;
+import org.apache.commons.lang3.StringUtils;
+
 
 /**
  * 
@@ -22,8 +32,10 @@ import io.netty.util.concurrent.GlobalEventExecutor;
  */
 public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
+	// @Autowired
+	// private UserServiceImpl userService;
 	// 用于记录和管理所有客户端的channle
-	private static ChannelGroup clients = 
+	public static ChannelGroup users = 
 			new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 	
 	
@@ -33,19 +45,89 @@ public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame>
 			throws Exception {
 		// 获取客户端传输过来的消息
 		String content = msg.text();
-		System.out.println("接受到的数据：" + content);
 		
-//		for (Channel channel: clients) {
-//			channel.writeAndFlush(
-//				new TextWebSocketFrame(
-//						"[服务器在]" + LocalDateTime.now() 
-//						+ "接受到消息, 消息为：" + content));
-//		}
-		// 下面这个方法，和上面的for循环，一致
-		clients.writeAndFlush(
-				new TextWebSocketFrame(
-						"[服务器在]" + LocalDateTime.now() 
-						+ "接受到消息, 消息为：" + content));
+		Channel currentChannel = ctx.channel();
+
+		// 1. 获取客户端发来的消息
+		DataContent dataContent = JsonUtils.jsonToPojo(content, DataContent.class);
+		Integer action = dataContent.getAction();
+		// 2. 判断消息类型，根据不同的类型来处理不同的业务
+
+		if (action == MsgActionEnum.CONNECT.type) {
+			// 	2.1  当websocket 第一次open的时候，初始化channel，把用的channel和userid关联起来
+			String senderId = dataContent.getChatMsg().getSenderId();
+			UserChannelRel.put(senderId, currentChannel);
+			
+			// 测试
+			for (Channel c : users) {
+				System.out.println(c.id().asLongText());
+			}
+			UserChannelRel.output();
+		} else if (action == MsgActionEnum.CHAT.type) {
+			//  2.2  聊天类型的消息，把聊天记录保存到数据库，同时标记消息的签收状态[未签收]
+			ChatMsg chatMsg = dataContent.getChatMsg();
+			String msgText = chatMsg.getMsg();
+			String receiverId = chatMsg.getReceiverId();
+			String senderId = chatMsg.getSenderId();
+			
+			// 保存消息到数据库，并且标记为 未签收
+			UserService userService = (UserService)SpringUtil.getBean("userServiceImpl");
+			String msgId = userService.saveMsg(chatMsg);
+			chatMsg.setMsgId(msgId);
+			
+			DataContent dataContentMsg = new DataContent();
+			dataContentMsg.setChatMsg(chatMsg);
+			
+			// 发送消息
+			// 从全局用户Channel关系中获取接受方的channel
+			Channel receiverChannel = UserChannelRel.get(receiverId);
+			if (receiverChannel == null) {
+				// TODO channel为空代表用户离线，推送消息（JPush，个推，小米推送）
+			} else {
+				// 当receiverChannel不为空的时候，从ChannelGroup去查找对应的channel是否存在
+				Channel findChannel = users.find(receiverChannel.id());
+				if (findChannel != null) {
+					// 用户在线
+					receiverChannel.writeAndFlush(
+							new TextWebSocketFrame(
+									JsonUtils.objectToJson(dataContentMsg)));
+				} else {
+					// 用户离线 TODO 推送消息
+					/**
+					 * 这里有很明显的问题，在用户id和channel对应的map里找到了channel，但是在channelgroup里没有找到
+					 * 那么应该是因为用户离线了，但是并没有清除原来的channel，做两个改进
+					 * 一、在用户离线，清楚channelGroup里channel的同时，清楚userchannelrel里的channel
+					 * 二、在这个地方添加清楚userchannelrel内channel的代码
+					 */
+				}
+			}
+			
+		} else if (action == MsgActionEnum.SIGNED.type) {
+			//  2.3  签收消息类型，针对具体的消息进行签收，修改数据库中对应消息的签收状态[已签收]
+			
+			UserService userService = (UserService)SpringUtil.getBean("userServiceImpl");
+			// 扩展字段在signed类型的消息中，代表需要去签收的消息id，逗号间隔
+			String msgIdsStr = dataContent.getExtand();
+			String msgIds[] = msgIdsStr.split(",");
+			
+			List<String> msgIdList = new ArrayList<>();
+			for (String mid : msgIds) {
+				if (StringUtils.isNotBlank(mid)) {
+					msgIdList.add(mid);
+				}
+			}
+			
+			System.out.println(msgIdList.toString());
+			
+			if (msgIdList != null && !msgIdList.isEmpty() && msgIdList.size() > 0) {
+				// 批量签收
+				userService.updateMsgSigned(msgIdList);
+			}
+			
+		} else if (action == MsgActionEnum.KEEPALIVE.type) {
+			//  2.4  心跳类型的消息
+			System.out.println("收到来自channel为[" + currentChannel + "]的心跳包...");
+		}
 		
 	}
 
@@ -55,16 +137,24 @@ public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame>
 	 */
 	@Override
 	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-		clients.add(ctx.channel());
+		users.add(ctx.channel());
 	}
 
 	@Override
 	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+		
+		String channelId = ctx.channel().id().asShortText();
+		System.out.println("客户端被移除，channelId为：" + channelId);
+		
 		// 当触发handlerRemoved，ChannelGroup会自动移除对应客户端的channel
-//		clients.remove(ctx.channel());
-		System.out.println("客户端断开，channle对应的长id为：" 
-							+ ctx.channel().id().asLongText());
-		System.out.println("客户端断开，channle对应的短id为：" 
-							+ ctx.channel().id().asShortText());
+		users.remove(ctx.channel());
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		// 发生异常之后关闭连接（关闭channel），随后从ChannelGroup中移除
+		ctx.channel().close();
+		users.remove(ctx.channel());
 	}
 }
